00acd4dc63d2b79b084f16cf7ab72b1f05330e02
aakosm <aakos.mate@gmail.com>
2023-06-22 15:08:18 +0200

    added documentation for na_count

--- R/na_count.R
+++ R/na_count.R
@@ -1,3 +1,12 @@
+#' Counting Missing Values
+#'
+#' @param x A vector or list
+#'
+#' @return Numerical vector
+#' @export
+#'
+#' @examples
+#' na_count(c(1, 2, NA, NA))
 na_count <- function(x) {
   base::sum(base::is.na(x))
 }
